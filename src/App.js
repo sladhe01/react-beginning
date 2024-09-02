db01436a779f7323be1e3c12990ac85bf8363bcb
sladhe01 <sladhe01@gmail.com>
2024-09-02 18:41:47 +0900

    practice complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import { useState } from "react";
+
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const onChange = (event) => {
+    setTodo(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (todo === "") {
+      return;
+    } else {
+      setTodos((prevTodos) => [...prevTodos, todo]);
+      setTodo("");
+    }
+  };
+  console.log(todos);
   return (
     <div>
-      <h1>Welcome back!</h1>
+      <h1>My Todos ({todos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input value={todo} onChange={onChange} type="text" placeholder="Write your to do..." />
+        <button>Add Todo</button>
+      </form>
+      <hr />
+      <ul>
+        {todos.map((todoList, index) => {
+          return <li key={index}>{todoList}</li>;
+        })}
+      </ul>
     </div>
   );
 }
